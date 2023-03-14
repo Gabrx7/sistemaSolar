a577be0cc3d00dc7cb8116255355a4a04ed1a6ed
Gabriel Rosinski <113986687+Gabrx7@users.noreply.github.com>
2023-03-14 08:42:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const btnMercurio = document.getElementById('btn-merc');
+const delButtonMerc = document.getElementById('button-merc')
+
+function btnMerc(){
+    delButtonMerc.remove();
+    btnMercurio.innerHTML = 'Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. Mercúrio completa três rotações em torno de seu eixo a cada duas órbitas. O periélio da órbita de Mercúrio apresenta uma precessão de 5 600 segundos de arco por século, um fenômeno completamente explicado apenas a partir do século XX pela Teoria da Relatividade Geral formulada por Albert Einstein.[2] A sua aparência é brilhante quando observado da Terra, tendo uma magnitude aparente que varia de −2,6 a 5,7, embora não seja facilmente observado pois sua separação angular do Sol é de apenas 28,3º. Uma vez que Mercúrio normalmente se perde no intenso brilho solar, exceto em eclipses solares, só pode ser observado a olho nu durante o crepúsculo matutino ou vespertino.';
+}
+
+const buttonVenus = document.getElementById('button-venus')
+const divVenus = document.getElementById('btn-venus')
+
+function btnVenus(){
+    buttonVenus.remove();
+    divVenus.innerHTML = "Vénus (português europeu) ou Vênus (português brasileiro) (AO 1990: Vénus ou Vênus)[1] é o segundo planeta do Sistema Solar em ordem de distância a partir do Sol, orbitando-o a cada 224,7 dias. Recebeu seu nome em homenagem à deusa romana do amor e da beleza Vénus, equivalente a Afrodite. Depois da Lua, é o objeto mais brilhante do céu noturno, atingindo uma magnitude aparente de -4,6, o suficiente para produzir sombras. A distância média da Terra a Vênus é de 0,28 AU, sendo esta a menor distância entre qualquer par de planetas.[2] Como Vénus se encontra mais próximo do Sol do que a Terra, ele pode ser visto aproximadamente na mesma direção do Sol (sua maior elongação é de 47,8°). Vénus atinge seu brilho máximo algumas horas antes da alvorada ou depois do ocaso, sendo por isso conhecido como a estrela da manhã (Estrela-d'Alva) ou estrela da tarde (Vésper); também é chamado Estrela do Pastor. Vénus é considerado um planeta do tipo terrestre ou telúrico, chamado com frequência de planeta irmão da Terra,[3] já que ambos são similares quanto ao tamanho, massa e composição. Vénus é coberto por uma camada opaca de nuvens de ácido sulfúrico altamente reflexivas, impedindo que a sua superfície seja vista do espaço na luz visível. Ele possui a mais densa atmosfera entre todos os planetas terrestres do Sistema Solar, constituída principalmente de dióxido de carbono. Vénus não possui um ciclo do carbono para fixar o carbono em rochas ou outros componentes da superfície, nem parece ter vida orgânica para absorvê-lo como biomassa. Acredita-se que no passado Vénus possuía oceanos como os da Terra,[4] que se evaporaram quando a temperatura se elevou, restando uma paisagem desértica, seca e poeirenta, com muitas pedras em forma de placas. A água provavelmente se dissociou e, devido à inexistência de um campo magnético, o hidrogênio foi arrastado para o espaço interplanetário pelo vento solar.[5] A pressão atmosférica na superfície do planeta é 92 vezes a da Terra. A superfície venusiana foi objeto de especulação até que alguns dos seus segredos foram revelados pela ciência planetária no século XX. Ele foi finalmente mapeado em detalhes pelo Programa Magellan de 1990 a 1994. O solo apresenta evidências de extenso vulcanismo e o enxofre na atmosfera pode indicar que houve algumas erupções recentes.[6][7] Entretanto, a falta de evidência de fluxo de lava acompanhando algumas das caldeiras visíveis permanece um enigma. O planeta possui poucas crateras de impacto, demonstrando que a superfície é relativamente jovem, com idade de aproximadamente 300-600 milhões de anos.[8][9] Não há evidência de placas tectônicas, possivelmente porque a crosta é muito forte para ser reduzida, sem água para torná-la menos viscosa. Em vez disso, Vénus pode perder seu calor interno em eventos periódicos de reposição da superfície."
+    ;
+}
+
+
+
+const buttonTerra = document.getElementById('button-terra')
+const divTerra = document.getElementById('btn-terra')
+
+function btnTerra(){
+    buttonTerra.remove();
+    divTerra.innerHTML = "A Terra é o terceiro planeta mais próximo do Sol, o mais denso e o quinto maior dos oito planetas do Sistema Solar. É também o maior dos quatro planetas telúricos. É por vezes designada como Mundo ou Planeta Azul. Lar de milhões de espécies de seres vivos,[13] incluindo os humanos, a Terra é o único corpo celeste onde é conhecida a existência de vida. O planeta formou-se há 4,56 bilhões de anos,[14][15][16][17] e a vida surgiu na sua superfície um bilhão de anos depois. Desde então, a biosfera terrestre alterou significativamente a atmosfera e outros fatores abióticos do planeta, permitindo a proliferação de organismos aeróbicos, bem como a formação de uma camada de ozônio, a qual, em conjunto com o campo magnético terrestre, bloqueia radiação solar prejudicial, permitindo a vida no planeta.[18] As propriedades físicas do planeta, bem como sua história geológica e órbita, permitiram que a vida persistisse durante este período. Acredita-se que a Terra poderá suportar vida durante pelo menos outros 500 milhões de anos"
+    ;
+}
+
+const buttonMarte = document.getElementById('button-marte')
+const divMarte = document.getElementById('btn-marte')
+
+function btnMarte(){
+    buttonMarte.remove();
+    divMarte.innerHTML = 'Marte é o quarto planeta a partir do Sol, o segundo menor do Sistema Solar. Batizado em homenagem a divindade romana da guerra, muitas vezes é descrito como o "Planeta Vermelho", porque o óxido de ferro predominante em sua superfície lhe dá uma aparência avermelhada.[1]';
+}
+
+const buttonJupter = document.getElementById('button-jupter')
+const divJupter = document.getElementById('btn-jupter')
+
+function btnJupter(){
+    buttonJupter.remove();
+    divJupter.innerHTML = "Júpiter é o maior planeta do Sistema Solar, estando situado entre Marte e Saturno. Seu tamanho rende-lhe vários satélites naturais orbitando ao seu redor, cerca de 70. Durante a noite, esse planeta pode ser visto a olho nu, sendo a segunda estrela mais brilhante, atrás apenas de Vênus, o segundo planeta na ordem usando-se o Sol como referência."
+}
+
+const buttonSaturno = document.getElementById('button-saturno')
+const divSaturno = document.getElementById('btn-saturno')
+
+function btnSaturno(){
+    buttonSaturno.remove();
+    divSaturno.innerHTML = "Saturno é o sexto planeta a partir do Sol e é o segundo maior planeta do Sistema Solar, ficando atrás apenas de Júpiter. Suas dimensões são 9 vezes maiores que as da Terra. Devido à sua composição, é conhecido também como gigante gasoso e destaca-se pela beleza do sistema de anéis que o circunda, os quais são formados a partir de fragmentos de rocha e gelo."
+}
+
+const buttonUrano = document.getElementById('button-urano')
+const divUrano = document.getElementById('btn-urano')
+
+function btnUrano(){
+    buttonUrano.remove();
+    divUrano.innerHTML = "Urano é o sétimo planeta do Sol e é o terceiro maior no sistema solar. Foi descoberto por William Herschel em 1781. Tem um diâmetro equatorial de 51.800 quilômetros (32.190 milhas) e orbita o Sol uma vez a cada 84,01 anos da Terra. Tem uma distância média do Sol de 2,87 bilhões quilômetros (1,78 bilhão milhas)."
+}
+
+const buttonNetuno = document.getElementById('button-netuno')
+const divNetuno = document.getElementById('btn-netuno')
+
+function btnNetuno(){
+    buttonNetuno.remove();
+    divNetuno.innerHTML = "Netuno ou Neptuno é o oitavo planeta do Sistema Solar, o último a partir do Sol desde a reclassificação de Plutão para a categoria de planeta anão, em 2006. Pertencente ao grupo dos gigantes gasosos, possui um tamanho ligeiramente menor que o de Urano, mas maior massa, equivalente a 17 massas terrestres."
+}
